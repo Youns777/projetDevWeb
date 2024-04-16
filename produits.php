d7d151faa3ac979dcc326e4a8e738fc685663a93
lazyass78 <lazizi.yassine78@gmail.com>
2024-04-16 15:42:25 +0200

    tentative remettre le produits style comme avant

--- produits.php
+++ produits.php
@@ -57,10 +57,10 @@ $produits = array_filter($produits, function($produit) use ($minPrix, $maxPrix, 
         <a href="panier.html"><img src="img/cart.png" class="cart"></a>
         <a href="login.html"><img src="img/login.png" class="login"></a>
         <table id="menu">
-        <tr>
-            <?php afficherMenu(); ?>
-        </tr>
-    </table>
+            <tr>
+                <?php afficherMenu(); ?>
+            </tr>
+        </table>
     </header>
 
     <aside>
@@ -90,20 +90,66 @@ $produits = array_filter($produits, function($produit) use ($minPrix, $maxPrix, 
     <div class="bloc1">
     <h2>Nos Produits <?php echo ucfirst($categorie); ?> :</h2>
     <section class="produits">
-    <?php foreach ($produits as $produit): ?>
-        <div class="chaussure">
-            <img src="<?php echo $produit['Image']; ?>" alt="produit">
-            <h3><?php echo $produit['Produit']; ?></h3>
-            <?php echo $produit['Prix']; ?>€<br>
-            <!-- ... -->
-            <div class="stock">
-                <p class="line_stock"> Stock : <?php echo $produit['Stock']; ?> </p>
-            </div>
-            <button class="ajouter-panier">Ajouter au panier</button>
-        </div>
-    <?php endforeach; ?>
-</section>
-</div>
+            <table>
+                <tr>
+                    <?php
+                    // Boucle pour chaque produit
+                    $count = 0;
+                    foreach ($produits as $produit):
+                    ?>
+                        <td class="chaussure">
+                            <img src="<?php echo $produit['Image']; ?>" alt="produit">
+                            <h3><?php echo $produit['Produit']; ?></h3>
+                            <?php echo $produit['Prix']; ?>€<br>
+                            Taille :
+                            <select name="taille">
+                                <?php
+                                
+                                $tailles_disponibles = [];  // tab des tailles
+
+                                switch ($categorie) {
+                                    case 'Homme':
+                                    case 'Femme':
+                                        $tailles_disponibles = range(40, 49);
+                                        break;
+                                    case 'Enfant':
+                                        $tailles_disponibles = range(35, 40);
+                                        break;
+                                    default:
+                                        $tailles_disponibles = range(35, 49);
+                                        break;
+                                }
+
+                                // Générer les options de la liste déroulante
+                                foreach ($tailles_disponibles as $taille) {
+                                    echo '<option value="' . $taille . '">' . $taille . '</option>';
+                                }
+                                ?>
+                            </select><br>
+                            Quantité :
+                            <select name="quantite" class="quantite">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                            <div class="stock">
+                                <p class="line_stock"> Stock : <?php echo $produit['Stock']; ?> </p>
+                            </div>
+                            <button class="ajouter-panier">Ajouter au panier</button>
+                        </td>
+                        <?php
+                        // Si le nombre de produits dans la ligne est un multiple de 4 ou c'est le dernier produit, fermez la ligne et commencez une nouvelle ligne
+                        $count++;
+                        if ($count % 4 == 0 || $count == count($produits)):
+                        ?>
+                </tr>
+                <?php endif; ?>
+                <?php endforeach; ?>
+            </table>
+        </section>
+    </div>
     <footer>
 
         <p>Copyright &copy; Société ShopTaSneakers<br>Webmaster CY Tech</p>
