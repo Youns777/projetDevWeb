989d9f5e7ec42875c2ad95e0c3c0f7f19e4f12d0
RyadZiouche <ryad.ziocuhe@gmail.com>
2024-04-25 16:05:56 +0200

    m

--- js/login_verif.js
+++ js/login_verif.js
@@ -5,9 +5,9 @@ let form = document.querySelector("#inscription_form");
 let nomInput = document.querySelector('input[name="nom"]');
 let prenomInput = document.querySelector('input[name="prenom"]');
 let emailInput = document.querySelector('input[name="email"]');
+let adresseInput = document.querySelector('input[name="adresse"]');
 let dateNaissanceInput = document.querySelector('input[name="Date de Naissance"]');
 let passwordInput = document.querySelector('input[name="password"]');
-let adresseInput = document.querySelector('input[name="adresse"]');
 
 // Ajout des écouteurs d'événements pour la validation des champs
 nomInput.addEventListener('input', function() {
@@ -22,6 +22,10 @@ emailInput.addEventListener('input', function() {
     validEmail(this.value);
 });
 
+adresseInput.addEventListener('input', function() {
+    validAdresse(this.value);
+});
+
 dateNaissanceInput.addEventListener('input', function() {
     validDateNaissance(this.value);
 });
@@ -30,24 +34,23 @@ passwordInput.addEventListener('input', function() {
     validPassword(this.value);
 });
 
-adresseInput.addEventListener('input', function() {
-    validAdresse(this.value);
-});
-
 // Fonction de validation de l'email
 const validEmail = function(inputEmail) {
     let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'i');
     let small = document.querySelector('#email_small');
+    let icon = document.querySelector('#email_icon');
 
     if (emailRegExp.test(inputEmail)) {
-        small.innerHTML = 'Adresse mail valide.';
+        small.innerHTML = ''; // Effacer le contenu
         small.classList.remove('text-danger');
         small.classList.add('text-success');
-        return true;
+        icon.style.display = 'inline'; 
+        return true; // Validation réussie
     } else {
         small.innerHTML = 'Adresse mail non valide !';
         small.classList.remove('text-success');
         small.classList.add('text-danger');
+        icon.style.display = 'none';
         return false;
     }
 };
@@ -56,74 +59,87 @@ const validEmail = function(inputEmail) {
 const validNom = function(inputNom) {
     let nomRegExp = new RegExp('^[A-Z][a-zA-Zéèê \-]+$', 'i');
     let small = document.querySelector('#nom_small');
+    let icon = document.querySelector('#nom_icon');
     
     if (inputNom.trim() === '') {
         small.innerHTML = 'Veuillez entrer votre nom.';
         small.classList.remove('text-success');
         small.classList.add('text-danger');
+        icon.style.display = 'none';
         return false; // Validation échouée
     } else if (nomRegExp.test(inputNom)) {
-        small.innerHTML = 'Nom valide.';
+        small.innerHTML = ''; // Effacer le contenu
         small.classList.remove('text-danger');
         small.classList.add('text-success');
+        icon.style.display = 'inline'; 
         return true; // Validation réussie
     } else {
         small.innerHTML = 'Nom non valide !';
         small.classList.remove('text-success');
         small.classList.add('text-danger');
+        icon.style.display = 'none'; 
         return false; // Validation échouée
     }
-}
+};
 
 // Fonction de validation du prénom
 const validPrenom = function(inputPrenom) {
     let prenomRegExp = new RegExp('^[A-Z][a-zA-Zéèê \-]+$', 'i');
     let small = document.querySelector('#prenom_small');
+    let icon = document.querySelector('#prenom_icon');
 
     if (inputPrenom.trim() === '') {
-        small.innerHTML = 'Veuillez entrer votre prénom.';
+        small.innerHTML = 'Veuillez entrer votre Prénom.';
         small.classList.remove('text-success');
         small.classList.add('text-danger');
-        return false;
+        icon.style.display = 'none';
+        return false; // Validation échouée
     } else if (prenomRegExp.test(inputPrenom)) {
-        small.innerHTML = 'Prénom valide.';
+        small.innerHTML = ''; 
         small.classList.remove('text-danger');
         small.classList.add('text-success');
-        return true;
+        icon.style.display = 'inline'; 
+        return true; 
     } else {
-        small.innerHTML = 'Prénom non valide !';
+        small.innerHTML = 'Prenom non valide !';
         small.classList.remove('text-success');
         small.classList.add('text-danger');
+        icon.style.display = 'none';
         return false;
     }
-}
+};
 
 // Fonction de validation de la date de naissance
 const validDateNaissance = function(inputDate) {
-    let dateNaissanceSmall = document.querySelector('#dateNaissance_small');
+    let small = document.querySelector('#dateNaissance_small');
+    let icon = document.querySelector('#dateNaissance_icon');
     let dateNaissance = new Date(inputDate);
     let currentDate = new Date();
     if (isNaN(dateNaissance.getTime())) {
-        dateNaissanceSmall.innerHTML = 'Veuillez entrer une date de naissance valide.';
-        dateNaissanceSmall.classList.remove('text-success');
-        dateNaissanceSmall.classList.add('text-danger');
+        small.innerHTML = 'Veuillez entrer une date de naissance.';
+        small.classList.remove('text-success');
+        small.classList.add('text-danger');
+        icon.style.display = 'none';
         return false;
     } else if (dateNaissance >= currentDate) {
-        dateNaissanceSmall.innerHTML = 'La date de naissance doit être antérieure à la date actuelle.';
-        dateNaissanceSmall.classList.remove('text-success');
-        dateNaissanceSmall.classList.add('text-danger');
+        small.innerHTML = 'La date de naissance doit être antérieure à la date actuelle.';
+        small.classList.remove('text-success');
+        small.classList.add('text-danger');
+        icon.style.display = 'none';
         return false;
     } else {
-        dateNaissanceSmall.innerHTML = 'Date de naissance valide.';
-        dateNaissanceSmall.classList.remove('text-danger');
-        dateNaissanceSmall.classList.add('text-success');
-        return true;
+        small.innerHTML = '';
+        small.classList.remove('text-danger');
+        small.classList.add('text-success');
+        icon.style.display = 'inline'; 
+        return true; // Validation réussie
     }
-}
+};
 
 // Fonction de validation du mot de passe
 const validPassword = function(inputPassword) {
     let passwordSmall = document.querySelector('#password_small');
+    let icon = document.querySelector('#password_icon');
     
     // Regex pour vérifier le format du mot de passe
     let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{8,}$/;
@@ -141,16 +157,19 @@ const validPassword = function(inputPassword) {
         passwordSmall.classList.add('text-danger');
         return false;
     } else {
-        passwordSmall.innerHTML = 'Mot de passe valide.';
-        passwordSmall.classList.remove('text-danger');
-        passwordSmall.classList.add('text-success');
+        small.innerHTML = '';
+        small.classList.remove('text-danger');
+        small.classList.add('text-success');
+        icon.style.display = 'inline';
         return true;
     }
-}
+};
 
 // Fonction de validation de l'adresse
 const validAdresse = function(inputAdresse) {
     let adresseSmall = document.querySelector('#adresse_small');
+    let icon = document.querySelector('#adresse_icon');
+    
     
     // Regex pour vérifier le format de l'adresse
     let adresseRegex = /([0-9]*) ?([a-zA-Z,\. ]*) ?([0-9]{5}) ?([a-zA-Z]*)/;
@@ -161,12 +180,13 @@ const validAdresse = function(inputAdresse) {
         adresseSmall.classList.add('text-danger');
         return false;
     } else {
-        adresseSmall.innerHTML = 'Adresse valide.';
-        adresseSmall.classList.remove('text-danger');
-        adresseSmall.classList.add('text-success');
+        small.innerHTML = '';
+        small.classList.remove('text-danger');
+        small.classList.add('text-success');
+        icon.style.display = 'inline';
         return true;
     }
-}
+};
 
 // Gestionnaire d'événements pour la soumission du formulaire
 form.addEventListener('submit', function(event) {
